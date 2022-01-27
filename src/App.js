ee96981360b19cc2cf1a9cffb253f4db4bba96d2
Sakshi031101 <81874214+Sakshi031101@users.noreply.github.com>
2022-01-27 15:52:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,188 @@
+
+import './App.css';
+import  './boy.png';
+import './vector.png';
+import './v2.png';
+import './page2.js';
+
+
+function App(){
+  return (
+    <div className="App">
+      <div className="App-header">
+      Announcements
+      </div>
+      
+      <div className="text">
+        Today
+      </div>
+      
+     <div className="rec3">
+           <div className="avatar">
+     
+           </div>
+      
+          <div className="t2">
+          
+            Saurabh Mantri <br/>
+            <div className="t3">
+                Class 4A 
+            </div>
+            
+          
+          
+            <div className="b2">
+            </div>
+          
+
+            <div className="badge">
+           
+               <div className="t6">
+                 Absent
+               </div>
+            </div>
+         </div>
+     </div>
+
+     <div className="rect4">
+           <div className="avatar">
+     
+           </div>
+      
+          <div className="t2">
+            Saurabh Mantri <br/>
+            <div className="t3">
+                Class 4A 
+            </div>
+            
+            <div className="textb2">
+            Unit Test
+            
+            <div className="textbb2">
+            English
+            </div>
+            </div>
+           
+            <div className="marks">
+            32 / 40
+            </div>
+
+              <div className="a1"></div>
+
+            <div className="badge">
+               <div className="c1">
+                 Marks
+               </div>
+            </div>
+            
+          </div>
+     
+     </div> 
+
+     <div className="text2">
+       Yesterday
+     </div>
+   
+     <div className="rect5">
+       <div className="avatar">
+     
+           </div>
+      
+          <div className="t2">
+            Saurabh Mantri <br/>
+            <div className="t3">
+                Class 4A 
+            </div>
+            
+            <div className="a2">
+            </div>
+          
+
+            <div className="badge">
+               <div className="c2">
+                 Fees
+               </div>
+            </div>
+            
+            <div className="bt3">
+            Due on: Thursday, 23 Nov 2021
+            </div>
+            
+          </div>
+     </div>
+
+     <div className="rect6">
+       <div className="avatar">
+     
+           </div>
+      
+          <div className="t2">
+            Saurabh Mantri <br/>
+            <div className="t3">
+                Class 4A 
+            </div>
+            
+            
+            <div className="a3">
+            </div>
+            
+
+            <div className="badge">
+               <div className="c3">
+                 Announcement
+               </div>
+            </div>
+
+            <div className="bt4">
+            School will be closed tomorrow due to rains <br/>
+            
+
+            <div className="bt41">
+            Any changes to school reopening will be <br/> 
+            </div>
+
+            <div className="bt42">
+            notified.
+            </div>
+
+            </div>
+
+            <div className="rule">
+            <hr/>
+            </div>
+
+            <div className="like">
+     
+           </div>
+
+           <div className="lt">
+             121
+           </div>
+
+
+
+           <div className="msg_pic">
+             
+           </div>
+
+           <div className="fb">
+           Send feedback
+           {/* Send feedback */}
+           </div>
+            
+           <div className="fbox">
+             
+           </div>   
+
+           <div className="ft">
+           Only School Admin will be able to see your feedback
+           </div> 
+
+          </div>
+     </div>
+     
+    </div>
+  );
+}
+
+export default App;
